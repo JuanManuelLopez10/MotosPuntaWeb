577683510dfc67d2f4237926e2f12497d519b2e9
Jotaeme10 <26lopez.jm@gmail.com>
2025-12-12 16:45:27 -0300

    Back to local backend 6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000",  // tu backend local
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        secure: false,
       },
     },
   },
